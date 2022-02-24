4117b4dba74a38c2e85e1d00c9dca5e429f7f9d8
rawbeen72 <75427075+rawbeen72@users.noreply.github.com>
2022-02-25 00:12:52 +0545

    Update _client.py

--- fbchat/_client.py
+++ fbchat/_client.py
@@ -206,7 +206,7 @@ class Client(object):
                     email,
                     password,
                     on_2fa_callback=self.on2FACode,
-                    user_agent=user_agent,
+                    user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
                 )
                 self._uid = self._state.user_id
             except Exception:
