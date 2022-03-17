928afe67f423dea7adb8448e48f0c402ce68f710
rawbeen72 <75427075+rawbeen72@users.noreply.github.com>
2022-03-17 11:12:19 +0545

    Update _mqtt.py

--- fbchat/_mqtt.py
+++ fbchat/_mqtt.py
@@ -274,13 +274,21 @@ class Mqtt(object):
         """
         rc = self._mqtt.loop(timeout=1.0)
         
-        if rc != paho.mqtt.client.MQTT_ERR_SUCCESS:
+ 
+        if self._mqtt._state == paho.mqtt.client.mqtt_cs_disconnecting:
+            return False  # Stop listening
+
+            if rc != paho.mqtt.client.MQTT_ERR_SUCCESS:
+            # If known/expected error
+                if rc == paho.mqtt.client.MQTT_ERR_CONN_LOST:
+                    log.warning("Connection lost, retrying")
+        elif rc == paho.mqtt.client.MQTT_ERR_NOMEM:
           
-            if rc == paho.mqtt.client.MQTT_ERR_NOMEM:
+           
                 # This error is wrongly classified
                 # See https://github.com/eclipse/paho.mqtt.python/issues/340
                 log.warning("Connection error, retrying")
-            elif rc == paho.mqtt.client.MQTT_ERR_CONN_REFUSED:
+            if rc == paho.mqtt.client.MQTT_ERR_CONN_REFUSED:
                 raise _exception.FBchatNotLoggedIn("MQTT connection refused")
             else:
                 err = paho.mqtt.client.error_string(rc)
